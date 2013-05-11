46d4549bfd61b043ab2fcd357c6db1b446a0b1a0
Charles Hill <charlesh@degreesofzero.com>
2013-05-10 21:53:02 -0500

    Removed PHP server side dependencies

--- application/helpers/cache_buster_helper.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function cache_buster($path)
-{
-	$file = BASE_DIR . $path;
-
-	return file_exists($file) ? '?' . sha1(filemtime($file)) : '';
-}
--- application/views/footer.php
+++
@@ -1,8 +0,0 @@
-
-
-<?php if (count($load_js) > 0): foreach ($load_js as $src): ?>
-
-	<script src="<?= $src ?>"></script><?php endforeach; endif; ?>
-
-</body>
-</html>
